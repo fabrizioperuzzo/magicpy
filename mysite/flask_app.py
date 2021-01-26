32a599c7038b21eeac89f628a81132318c1c36a7
fabrizioperuzzo <fabrperu@gmail.com>
2021-01-26 11:07:42 +0100

    webhook2

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        repo = git.Repo('https://github.com/fabrizioperuzzo/magicpy')
+        repo = git.Repo('https://github.com/fabrizioperuzzo/magicpy.git')
         origin = repo.remotes.origin
         origin.pull()
         return 'Updated PythonAnywhere successfully', 200
